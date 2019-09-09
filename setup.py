068e1ad14a7b179133d66065abf0100d5507a3b9
neeraj059 <55087664+neeraj059@users.noreply.github.com>
2019-09-09 16:31:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
     install_requires=[
         'sqlparse==0.2.4',
         'pymongo==3.5.0',
-        'django>=2.0',
+        'django==1.11.24',
         'dataclasses>=0.1',
     ],
     extras_require=dict(
