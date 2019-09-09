7f9140818ac4bbfa40c8407b2f24fba1d6a723a8
neeraj059 <55087664+neeraj059@users.noreply.github.com>
2019-09-09 16:19:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -90,8 +90,8 @@ setup(
     description=(
         'Driver for allowing Django to use MongoDB as the database backend.'),
     install_requires=[
-        'sqlparse>=0.2.3',
-        'pymongo>=3.2.0',
+        'sqlparse==0.2.4',
+        'pymongo==3.5.0',
         'django>=2.0',
         'dataclasses>=0.1',
     ],
